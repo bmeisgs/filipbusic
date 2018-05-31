e1143952520c339fd882a7ea3ed6aa6fc6345a76
Filip <Filip@DESKTOP-ES4MJCE>
2018-05-31 13:15:28 +0200

    Finished HW.

---
+++ variables.js
@@ -0,0 +1,10 @@
+let int = 2;
+let boolean = true;
+let string = "I have no fu**ing time";
+
+let array = [int, boolean, string];
+
+let object = {int: int, boolean: boolean, string: string};
+
+console.log(array);
+console.log(object);
